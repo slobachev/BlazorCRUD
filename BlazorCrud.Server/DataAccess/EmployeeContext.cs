f70bcc35a02182774cef865cb0150620594effb8
Sergey Lobachev <SLobachev@Verismic.com>
2018-07-05 17:21:57 +0300

    Add pages and models

---
+++ BlazorCrud.Server/DataAccess/EmployeeContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorCrud.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.DataAccess
+{
+    public class EmployeeContext : DbContext
+    {
+        public virtual DbSet<Employee> tblEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=VKYV-DT-SL;Initial Catalog=4Fun;Integrated Security=True");
+            }
+        }
+    }
+}
