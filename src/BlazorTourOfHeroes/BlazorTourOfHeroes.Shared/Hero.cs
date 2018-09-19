db0bf6a1405f9b9de1d4e05156809952bf3d2da6
Sergey Lobachev <sergeylobachev@outlook.com>
2018-09-19 18:01:24 +0300

    initial commit

---
+++ src/BlazorTourOfHeroes/BlazorTourOfHeroes.Shared/Hero.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTourOfHeroes.Shared
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
